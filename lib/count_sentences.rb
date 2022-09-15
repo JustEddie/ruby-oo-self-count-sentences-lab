29df577128502e9c3d456e9252abb8f77b759609
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-09-15 19:02:24 +1000

    commit tree 3ba37b11daf82bd46e5ae0a9f3a05dce20f94ff7

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,34 @@ require 'pry'
 
 class String
 
+
+  attr_accessor :sen
+
   def sentence?
-    
+    if self[self.length-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[self.length-1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[self.length-1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!!/).count
   end
 end
